eb69dbc30e6efe704d30412875d10957d5f87b2a
Dinhero21 <78858626+Dinhero21@users.noreply.github.com>
2023-03-06 18:29:15 -0300

    Entity Relationship

--- src/public/engine/entities/base.ts
+++ src/public/engine/entities/base.ts
@@ -1,15 +1,41 @@
-import type Frame from '../frame.js'
+import Frame from '../frame.js'
 import Vec2 from '../vec2.js'
 
 export class Entity {
+  private readonly offset = Math.random()
+
+  private frame = 0
+
   public position: Vec2 = new Vec2(0, 0)
 
+  // Entity relationship
+
+  public children: Entity[] = []
+
   public draw (frame: Frame): void {
+    this.frame++
+
     frame.drawFancyRectRGBA(0, 0, 256, 256, 0x61, 0xAF, 0xEF)
+
+    for (const child of this.children) {
+      const childFrame = new Frame()
+      childFrame.offset = child.position
+
+      child.draw(childFrame)
+
+      childFrame.draw(frame)
+    }
   }
 
   public update (delta: number): void {
-    this.position.x += delta * 10
+    for (const child of this.children) {
+      child.update(delta)
+    }
+
+    this.position.set(
+      (Math.sin(Date.now() / 1000) + 1) * 100,
+      (Math.cos(Date.now() / 1000) + 1) * 100
+    )
   }
 }
 
