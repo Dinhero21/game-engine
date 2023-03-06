b6b925af856bfd61ddd80541f434bc1fbb99a828
Dinhero21 <78858626+Dinhero21@users.noreply.github.com>
2023-03-05 22:15:58 -0300

    Webserver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+import express from 'express'
+import path from 'path'
+
+const app = express()
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.listen(80)
