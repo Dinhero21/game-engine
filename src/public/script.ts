45f78655b9091d027d1453e2da913c2ab11cf7ac
Dinhero21 <78858626+Dinhero21@users.noreply.github.com>
2023-03-06 15:08:57 -0300

    View

---
+++ src/public/script.ts
@@ -0,0 +1,24 @@
+const view = document.getElementById('view')
+
+if (view === null) throw new Error('Could not find the view element')
+if (!(view instanceof HTMLCanvasElement)) throw new Error('The view element is not a canvas element')
+
+const context = view.getContext('2d')
+
+if (context === null) throw new Error('Could not get view context')
+
+updateViewSize()
+
+window.addEventListener('resize', updateViewSize)
+
+function updateViewSize (): void {
+  if (view === null) throw new Error('updateViewSize called with view being null')
+  if (!(view instanceof HTMLCanvasElement)) throw new Error('updateViewSize called with context not being not being a canvas element')
+  if (context === null) throw new Error('updateViewSize called with context being null')
+
+  view.width = window.innerWidth
+  view.height = window.innerHeight
+
+  context.fillStyle = `rgba(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},1)`
+  context.fillRect(0, 0, view.width, view.height)
+}
