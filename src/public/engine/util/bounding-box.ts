13c27edd14ede9f0a2fe78c3827d9a2fdc771bbe
Dinhero21 <78858626+Dinhero21@users.noreply.github.com>
2023-03-08 19:22:35 -0300

    Modularer BoundingBox

--- src/public/engine/util/bounding-box.ts
+++ src/public/engine/util/bounding-box.ts
@@ -28,38 +28,96 @@ export class BoundingBox {
     this.size = size
   }
 
-  // ? Should I make this public or protected (or private)?
-  public distancesTo (other: BoundingBox): number[] {
+  public _distanceLeft (other: BoundingBox): number {
     const start = this.getStart()
+    // const end = this.getEnd()
+
+    // const otherStart = other.getStart()
+    const otherEnd = other.getEnd()
+
+    return start.x - otherEnd.x
+  }
+
+  public distanceLeft (other: BoundingBox): number {
+    return Math.max(this._distanceLeft(other), other._distanceRight(this))
+  }
+
+  public _distanceRight (other: BoundingBox): number {
+    // const start = this.getStart()
     const end = this.getEnd()
 
     const otherStart = other.getStart()
+    // const otherEnd = other.getEnd()
+
+    return otherStart.x - end.x
+  }
+
+  public distanceRight (other: BoundingBox): number {
+    return Math.max(this._distanceRight(other), other._distanceLeft(this))
+  }
+
+  public _distanceDown (other: BoundingBox): number {
+    const start = this.getStart()
+    // const end = this.getEnd()
+
+    // const otherStart = other.getStart()
     const otherEnd = other.getEnd()
 
+    return start.y - otherEnd.y
+  }
+
+  public distanceDown (other: BoundingBox): number {
+    return Math.max(this._distanceDown(other), other._distanceUp(this))
+  }
+
+  public _distanceUp (other: BoundingBox): number {
+    // const start = this.getStart()
+    const end = this.getEnd()
+
+    const otherStart = other.getStart()
+    // const otherEnd = other.getEnd()
+
+    return otherStart.y - end.y
+  }
+
+  public distanceUp (other: BoundingBox): number {
+    return Math.max(this._distanceUp(other), other._distanceDown(this))
+  }
+
+  public distances (other: BoundingBox): { left: number, right: number, down: number, up: number } {
+    const left = this._distanceLeft(other)
+    const right = this._distanceRight(other)
+    const down = this._distanceDown(other)
+    const up = this._distanceUp(other)
+
+    return { left, right, down, up }
+  }
+
+  public distanceArray (other: BoundingBox): [number, number, number, number] {
     return [
-      start.x - otherEnd.x,
-      otherStart.x - end.x,
-      start.y - otherEnd.y,
-      otherStart.y - end.y
+      this._distanceLeft(other),
+      this._distanceRight(other),
+      this._distanceUp(other),
+      this._distanceDown(other)
     ]
   }
 
-  public distanceTo (other: BoundingBox): number {
-    const distances = this.distancesTo(other)
+  public distance (other: BoundingBox): number {
+    const distances = this.distanceArray(other)
 
     return Math.max(...distances)
   }
 
   public touching (other: BoundingBox): boolean {
-    return this.distanceTo(other) === 0
+    return this.distance(other) === 0
   }
 
   public overlapping (other: BoundingBox): boolean {
-    return this.distanceTo(other) < 0
+    return this.distance(other) < 0
   }
 
   public colliding (other: BoundingBox): boolean {
-    return this.overlapping(other) || this.touching(other)
+    return this.distance(other) <= 0
   }
 }
 
