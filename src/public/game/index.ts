9aea7cb7b45fe9955c8c0872387e489476cd45d2
Dinhero21 <78858626+Dinhero21@users.noreply.github.com>
2023-04-20 17:59:14 -0300

    Re-added Animation

--- src/public/game/index.ts
+++ src/public/game/index.ts
@@ -2,7 +2,7 @@ import type { IClientSocket as Socket } from '../../socket.io.js'
 import type Tile from '../engine/util/tilemap/tile.js'
 import Vec2, { stringToVec2 } from '../engine/util/vec2.js'
 import { CHUNK_SIZE, positionToTilePosition, tilePositionToChunkPosition, TILE_SIZE } from '../engine/util/tilemap/position-conversion.js'
-// import { animatePosition, TRANSFORMATIONS } from '../engine/patchers/animate.js'
+import { animatePosition, TRANSFORMATIONS } from '../engine/patchers/animate.js'
 import { ViewportGenerators } from '../engine/camera.js'
 import { Chunk } from '../engine/util/tilemap/chunk.js'
 import { loader } from '../assets/loader.js'
@@ -16,7 +16,6 @@ import Loop from '../engine/util/loop.js'
 import MultiplayerContainerEntity from './entities/multiplayer-container.js'
 import TileMapEntity from '../engine/entities/tilemap.js'
 import DebugEntity from './entities/debug.js'
-import { patchEntity } from '../engine/patchers/index.js'
 
 const chunkSize = new Vec2(CHUNK_SIZE, CHUNK_SIZE)
 
@@ -152,130 +151,36 @@ export default function createScene (context: CanvasRenderingContext2D): Scene {
     }
   })()
 
-  // {
-  //   const linearAnimationDebug = new DebugEntity('Animation (Linear)')
-  //   scene.addChild(linearAnimationDebug)
+  {
+    const linearAnimationDebug = new DebugEntity('Animation (Linear)')
+    scene.addChild(linearAnimationDebug)
 
-  //   const quadraticAnimationDebug = new DebugEntity('Animation (Quadratic)')
-  //   scene.addChild(quadraticAnimationDebug)
+    const quadraticAnimationDebug = new DebugEntity('Animation (Quadratic)')
+    scene.addChild(quadraticAnimationDebug)
 
-  //   const cubicAnimationDebug = new DebugEntity('Animation (Cubic)')
-  //   scene.addChild(cubicAnimationDebug)
+    const cubicAnimationDebug = new DebugEntity('Animation (Cubic)')
+    scene.addChild(cubicAnimationDebug)
 
-  //   const quarticAnimationDebug = new DebugEntity('Animation (Quartic)')
-  //   scene.addChild(quarticAnimationDebug)
+    const quarticAnimationDebug = new DebugEntity('Animation (Quartic)')
+    scene.addChild(quarticAnimationDebug)
 
-  //   const quinticAnimationDebug = new DebugEntity('Animation (Quintic)')
-  //   scene.addChild(quinticAnimationDebug)
+    const quinticAnimationDebug = new DebugEntity('Animation (Quintic)')
+    scene.addChild(quinticAnimationDebug)
 
-  //   void (async () => {
-  //     while (true) {
-  //       await Promise.all([
-  //         animatePosition(linearAnimationDebug, new Vec2(512, 0), new Vec2(512 + 64, 0), 1, TRANSFORMATIONS.Linear),
-  //         animatePosition(quadraticAnimationDebug, new Vec2(512, -128), new Vec2(512 + 64, -128), 1, TRANSFORMATIONS.EaseQuadratic),
-  //         animatePosition(cubicAnimationDebug, new Vec2(512, -256), new Vec2(512 + 64, -256), 1, TRANSFORMATIONS.EaseCubic),
-  //         animatePosition(quarticAnimationDebug, new Vec2(512, -384), new Vec2(512 + 64, -384), 1, TRANSFORMATIONS.EaseQuartic),
-  //         animatePosition(quinticAnimationDebug, new Vec2(512, -512), new Vec2(512 + 64, -512), 1, TRANSFORMATIONS.EaseQuintic)
-  //       ])
-  //     }
-  //   })()
-  // }
+    void (async () => {
+      while (true) {
+        await Promise.all([
+          animatePosition(linearAnimationDebug, new Vec2(512, 0), new Vec2(512 + 64, 0), 1, TRANSFORMATIONS.Linear),
+          animatePosition(quadraticAnimationDebug, new Vec2(512, -128), new Vec2(512 + 64, -128), 1, TRANSFORMATIONS.EaseQuadratic),
+          animatePosition(cubicAnimationDebug, new Vec2(512, -256), new Vec2(512 + 64, -256), 1, TRANSFORMATIONS.EaseCubic),
+          animatePosition(quarticAnimationDebug, new Vec2(512, -384), new Vec2(512 + 64, -384), 1, TRANSFORMATIONS.EaseQuartic),
+          animatePosition(quinticAnimationDebug, new Vec2(512, -512), new Vec2(512 + 64, -512), 1, TRANSFORMATIONS.EaseQuintic)
+        ])
+      }
+    })()
+  }
 
   scene.addChild(mouseDebug)
-
-  const testEntity1 = new DebugEntity('Test (1)')
-  scene.addChild(testEntity1)
-
-  testEntity1.position.set(0, -256)
-
-  // Wait 3 seconds
-  setTimeout(() => {
-    // Move 100px/s to the right for 1s
-    patchEntity(testEntity1)('update', helper => {
-      setTimeout(() => { helper.restore() }, 1 * 1000)
-
-      return function (delta: number) {
-        this.position.x += delta * 100
-
-        helper.below(delta)
-      }
-    })
-
-    // Move 100px/s up for 3s
-    patchEntity(testEntity1)('update', helper => {
-      setTimeout(() => { helper.restore() }, 3 * 1000)
-
-      return function (delta: number) {
-        this.position.y -= delta * 100
-
-        helper.below(delta)
-      }
-    })
-
-    // Wait 2 seconds
-    setTimeout(() => {
-      // Move in a circle at 60 rpm indefinitely
-      patchEntity(testEntity1)('update', helper => {
-        let t = 0
-
-        return function (delta: number) {
-          t += delta
-
-          this.position.x += Math.sin(2 * Math.PI * t)
-          this.position.y += Math.cos(2 * Math.PI * t)
-
-          helper.below(delta)
-        }
-      })
-    }, 2 * 1000)
-  }, 3 * 1000)
-
-  const testEntity2 = new DebugEntity('Test (2)')
-  scene.addChild(testEntity2)
-
-  testEntity2.position.set(256, -256)
-
-  // Wait 3 seconds
-  setTimeout(() => {
-    // Move 100px/s to the right for 1s
-    patchEntity(testEntity2)('update', helper => {
-      setTimeout(() => { helper.restore() }, 1 * 1000)
-
-      return function (delta: number) {
-        this.position.x -= delta * 100
-
-        helper.below(delta)
-      }
-    })
-
-    // Move 100px/s up for 3s
-    patchEntity(testEntity2)('update', helper => {
-      setTimeout(() => { helper.restore() }, 3 * 1000)
-
-      return function (delta: number) {
-        this.position.y += delta * 100
-
-        helper.below(delta)
-      }
-    })
-
-    // Wait 2 seconds
-    setTimeout(() => {
-      // Move in a circle at 60 rpm indefinitely
-      patchEntity(testEntity2)('update', helper => {
-        let t = 0
-
-        return function (delta: number) {
-          t += delta
-
-          this.position.x += Math.sin(2 * Math.PI * t)
-          this.position.y += Math.cos(2 * Math.PI * t)
-
-          helper.below(delta)
-        }
-      })
-    }, 2 * 1000)
-  }, 3 * 1000)
 
   return scene
 }
