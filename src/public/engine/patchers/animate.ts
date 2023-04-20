8b724b215d5700c05381aedfd99e4af2b39e0846
Dinhero21 <78858626+Dinhero21@users.noreply.github.com>
2023-04-20 17:55:19 -0300

    Even Better Entity Patching

--- src/public/engine/patchers/animate.ts
+++ src/public/engine/patchers/animate.ts
@@ -1,52 +1,52 @@
-import type Entity from '../entities/index.js'
-import { patchEntity } from './index.js'
-import type Vec2 from '../util/vec2.js'
-import { lerp2D } from '../util/math.js'
+// import type Entity from '../entities/index.js'
+// import { patchEntity } from './index.js'
+// import type Vec2 from '../util/vec2.js'
+// import { lerp2D } from '../util/math.js'
 
-export async function animate (entity: Entity, callback: (t: number) => void, duration: number): Promise<void> {
-  let t = 0
+// export async function animate (entity: Entity, callback: (t: number) => void, duration: number): Promise<void> {
+//   let t = 0
 
-  await new Promise<void>(resolve => {
-    callback(t)
+//   await new Promise<void>(resolve => {
+//     callback(t)
 
-    patchEntity(entity)('update', (helper, delta) => {
-      helper.original(delta)
+//     patchEntity(entity)('update', (helper, delta) => {
+//       helper.below(delta)
 
-      t += delta
+//       t += delta
 
-      if (t >= duration) {
-        t = duration
+//       if (t >= duration) {
+//         t = duration
 
-        callback(t)
+//         callback(t)
 
-        helper.restore()
+//         helper.restore()
 
-        resolve()
+//         resolve()
 
-        return
-      }
+//         return
+//       }
 
-      callback(t)
-    })
-  })
-}
+//       callback(t)
+//     })
+//   })
+// }
 
-export type Transformation = (x: number) => number
+// export type Transformation = (x: number) => number
 
-export const TRANSFORMATIONS: Record<string, Transformation> = {
-  Linear: x => x,
-  EaseQuadratic: x => x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2,
-  EaseCubic: x => x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2,
-  EaseQuartic: x => x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2,
-  EaseQuintic: x => x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2
-}
+// export const TRANSFORMATIONS: Record<string, Transformation> = {
+//   Linear: x => x,
+//   EaseQuadratic: x => x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2,
+//   EaseCubic: x => x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2,
+//   EaseQuartic: x => x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2,
+//   EaseQuintic: x => x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2
+// }
 
-export async function animatePosition<T extends Entity> (entity: T, start: Vec2, end: Vec2, duration: number, transformation: Transformation = TRANSFORMATIONS.Linear): Promise<void> {
-  await animate(entity, t => {
-    const delta = transformation(t / duration)
+// export async function animatePosition<T extends Entity> (entity: T, start: Vec2, end: Vec2, duration: number, transformation: Transformation = TRANSFORMATIONS.Linear): Promise<void> {
+//   await animate(entity, t => {
+//     const delta = transformation(t / duration)
 
-    const position = lerp2D(start, end, delta)
+//     const position = lerp2D(start, end, delta)
 
-    entity.position = position
-  }, duration)
-}
+//     entity.position = position
+//   }, duration)
+// }
--- src/public/engine/patchers/index.ts
+++ src/public/engine/patchers/index.ts
@@ -25,35 +25,89 @@ patch(entity)('update', (helper, delta) => {
 })
 */
 
+type PatchedEntities<T extends Entity = Entity, K extends keyof T = keyof T> = Map<T, Map<K, Set<T[K]>>>
+
+const patchedEntities: PatchedEntities = new Map()
+
 // TODO: Make a generic patch function that can patch methods not present on Entity (FunctionKeys<T extends Entity> does not work for some reason)
-// TODO: Make this function return explicit return types (too bored ngl)
+// TODO: Make this function return explicit return types (eslint won't do it automatically and too bored to do it manually)
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function patchEntity (entity: Entity) {
-  interface PatchHelper<Original> {
-    original: Original
+export function patchEntity<T extends Entity> (entity: T) {
+  interface PatchHelper<Below extends AnyFunction> {
+    below: (this: ThisParameterType<Below>, ...args: Parameters<Below>) => ReturnType<Below>
     restore: () => void
   }
 
-  return <PropertyName extends FunctionKeys<Entity> = FunctionKeys<Entity>>(property: PropertyName, callback: (this: Entity, test: PatchHelper<Entity[PropertyName]>, ...args: MaybeParameters<Entity[PropertyName]>) => MaybeReturnType<Entity[PropertyName]>): void => {
-    let original = entity[property]
+  return <PropertyName extends FunctionKeys<Entity>>(property: PropertyName, callback: (this: T, helper: PatchHelper<Entity[PropertyName]>) => ((this: T, ...args: Parameters<Entity[PropertyName]>) => ReturnType<Entity[PropertyName]>)): void => {
+    const original = entity[property]
 
     // ? Should I return or throw an error?
     if (!isFunction(original)) throw new Error(`Attempted to patch a non-function property ${JSON.stringify(property)} of type ${JSON.stringify(typeof original)}`)
 
     type Original = typeof original
+    type OriginalArgs = Parameters<Original>
 
-    original = original.bind(entity) as Original
+    let patchedEntity = patchedEntities.get(entity)
 
-    (entity[property] as MaybeFunction<Entity[PropertyName]>) = (...args) => {
-      return callback.apply(entity, [
-        {
-          original,
-          restore () {
-            entity[property] = original
-          }
-        },
-        ...args
-      ])
+    if (patchedEntity === undefined) {
+      patchedEntity = new Map()
+      patchedEntities.set(entity, patchedEntity)
     }
+
+    let patches = patchedEntity.get(property) as Set<Original>
+
+    if (patches === undefined) {
+      patches = new Set()
+      patchedEntity.set(property, patches)
+    }
+
+    patches.add(original)
+
+    const helper: PatchHelper<Original> = {
+      below (...args: OriginalArgs): ReturnType<Original> {
+        let index = 0
+
+        if (patches === undefined) throw new Error('Helper.below called before initialization phase')
+
+        for (const entityPatch of patches) {
+          if (entityPatch === patch) break
+
+          index++
+        }
+
+        const below = Array.from(patches)[index - 1]
+
+        if (below === undefined) throw new Error('Invalid patch ID')
+        if (!isFunction(below)) throw new Error('Invalid below')
+
+        return (below as ((...args: OriginalArgs) => ReturnType<Original>)).apply(entity, args)
+      },
+      restore () {
+        throw new Error('Helper.restore called before initialization phase')
+      }
+    }
+
+    const patch = callback.apply(entity, [helper])
+
+    // ? Should I bind patch to entity here?
+
+    helper.restore = () => {
+      // If I (patch) am the last one, restore entity[property] to its "lower" state
+
+      const patchArray = Array.from(patches)
+      const index = patchArray.findIndex(p => p === patch)
+
+      // If I (patch) am the last one
+      if (index === patchArray.length - 1) {
+        // Restore entity[property] to its "lower" state
+        entity[property] = patchArray[index - 1]
+      }
+
+      patches.delete(patch as Original)
+    }
+
+    patches.add(patch as Original)
+
+    entity[property] = patch as Original
   }
 }
