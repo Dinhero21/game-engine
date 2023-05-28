9bfc18c95dc0b98c61f3ce5e4265fa8e847cf63c
Dinhero21 <78858626+Dinhero21@users.noreply.github.com>
2023-05-28 13:06:53 -0300

    Prettier errors

--- src/public/game/index.ts
+++ src/public/game/index.ts
@@ -1,15 +1,17 @@
 import type { IClientSocket as Socket } from '../../socket.io.js'
 import { ViewportGenerators } from '../engine/camera.js'
 import Scene from '../engine/scene.js'
+import { positionToTilePosition } from '../engine/util/tilemap/position-conversion.js'
 import io from '../socket.io/socket.io.esm.min.js'
 import Loop from '../engine/util/loop.js'
 import DebugEntity from './entities/debug.js'
 import WorldEntity from './entities/world.js'
 import MultiplayerContainerEntity from './entities/multiplayer-container.js'
 import mouse from '../engine/util/input/mouse.js'
-import { positionToTilePosition } from '../engine/util/tilemap/position-conversion.js'
 
 export default function createScene (context: CanvasRenderingContext2D): Scene {
+  let running = true
+
   const socket: Socket = io()
 
   socket.on('disconnect', reason => {
@@ -33,7 +35,19 @@ export default function createScene (context: CanvasRenderingContext2D): Scene {
 
   // Instant = Fastest Javascript Allows
   Loop.instant()(delta => {
-    scene.update(delta)
+    if (!running) return
+
+    try {
+      scene.update(delta)
+    } catch (error) {
+      if (!(error instanceof Error)) throw error as any
+
+      prettyPrintError(error)
+
+      running = false
+
+      throw error
+    }
 
     const mousePosition = scene.getMouseViewportPosition()
 
@@ -42,7 +56,19 @@ export default function createScene (context: CanvasRenderingContext2D): Scene {
 
   // Draw = Animation Frames
   Loop.draw()(delta => {
-    camera.render()
+    if (!running) return
+
+    try {
+      camera.render()
+    } catch (error) {
+      if (!(error instanceof Error)) throw error as any
+
+      prettyPrintError(error)
+
+      running = false
+
+      throw error
+    }
   })
 
   const world = new WorldEntity(socket)
@@ -68,4 +94,30 @@ export default function createScene (context: CanvasRenderingContext2D): Scene {
   scene.addChild(mouseDebug)
 
   return scene
+
+  function prettyPrintError (error: Error): void {
+    let i = 0
+    let linesDrawn = 0
+
+    Loop.draw()(delta => {
+      i += delta * 36
+
+      context.font = '32px cursive'
+      context.fillStyle = `hsl(${i},100%,50%)`
+
+      const stack = error.stack ?? 'undefined'
+      const lines = stack.split('\n')
+
+      for (let i = 0; i < lines.length && i < linesDrawn; i++) {
+        const line = lines[i]
+        const y = (i + 1) * 32
+
+        context.fillText(line, 0, y)
+      }
+    })
+
+    setInterval(() => {
+      linesDrawn++
+    }, 10)
+  }
 }
